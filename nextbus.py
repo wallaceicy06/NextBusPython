542a50fef55ddff1972da12fbf35e1457957a209
Sean Harger <seanharger93@gmail.com>
2015-09-12 22:33:36 -0700

    Added lots of documentation.
    I feel like a basically paraphrased the NextBus API. :/

--- nextbus.py
+++ nextbus.py
@@ -6,6 +6,15 @@ BASE_URL = "http://webservices.nextbus.com/service/publicXMLFeed"
 JAVA_TF_TO_PYTHON_TF = { 'true': True, 'false': False }
 
 def get_agencies():
+    '''
+    Provides all the agencies that can be used with NextBus.
+
+    Returns:
+        a dictionary of agencies indexed by their agency id. Each value is the
+        respective region, title, and (if applicable) short title for that
+        agency.
+    '''
+
     res_agencies = requests.get(BASE_URL, {'command': 'agencyList'})
 
     agency_tree = ET.fromstring(res_agencies.text)
@@ -23,11 +32,29 @@ def get_agencies():
     return agency_dict
 
 class Agency:
+    '''
+    Representation of a NextBus Transit Agency. To interact with routes and
+    live predictions, create an Agency object by specifying the agency id.
+    '''
+
     def __init__(self, agency):
+        '''
+        Constructs an Agency.
+
+        Arguments:
+            agency - the string id for the agency
+        '''
         self.agency = agency
         self.routes = self.get_routes()
 
     def get_routes(self):
+        '''
+        Determines the routes that compose this transit agency.
+
+        Returns:
+            a dictionary of routes, where each key is the route id and each
+            value is the title of the route
+        '''
         res_routes = requests.get(BASE_URL,
           { 'a': self.agency, 'command': 'routeList' })
 
@@ -42,6 +69,26 @@ class Agency:
         return routes_dict
 
     def get_route_config(self, route_id):
+        '''
+        Determines the route configuration for a particular route for this
+        agency.
+
+        Arguments:
+            route_id - a string that uniquely identifies the route for this
+              agency
+
+        Returns:
+            a dictionary with the following keys and values:
+                
+                * stops: a dictionary of stops, each indexed by their
+                    stop tag and containing latitude (lat), longitude (lon),
+                    title, and (optionally) stopId, shortTitle
+                * directions: a dictionary of directions for this route,
+                    indexed by the unique direction tag and containing a
+                    title attribute, an optional additional name, as well as 
+                    an ordered list of stop tags for that direction
+                * paths: currently unused
+        '''
         if route_id not in self.routes:
             raise Exception('The specified route is invalid.')
 
@@ -87,7 +134,52 @@ class Agency:
 
         return route_cfg_dict
 
-    def predictions(self, stop_id, route_id=None):
+    def get_predictions(self, stop_id, route_id=None):
+        '''
+        Determines predictions for next arrivals of at a particular stop.
+
+        Arguments:
+            stop_id - the unique string stopId for the stop
+            route_id (optional) - the unique string routeId to return
+              predictions for
+
+        Returns:
+            If no specific route is specified, then a dictionary of predictions
+            indexed by routeId is returned, each containing the data below.
+
+            If a specific route is specified, only one dictionary will be
+            returned, containing the data below.
+
+                * directions: a dictionary of directions indexed by their
+                    direction tag and containing a list of prediction elements.
+                    Each prediction is a dictionary with the following
+                    attributes regarding the prediction of a particular
+                    vehicle:
+                    
+                    - epochTime: the estimated arrival time of the vehicle
+                        (in epoch time)
+                    - seconds: the number of seconds until the arrival of this
+                        vehicle
+                    - minutes: the number of minutes until the arrival of this
+                        vehicle
+                    - isDeparture: true if this is a departure prediction,
+                        false if it is an arrival prediction
+                    - dirTag: the unique direction tag for this prediction
+                    - tripTag: the unique trip tag for this prediction
+                    - affectedByLayover: whether or not this prediction is
+                        subject to an existing or pending layover
+                    - isScheduleBased: whether or not this prediction is based
+                        on a schedule (true) or real-time (false)
+                    - isDelayed: whether or not this prediction is for a vehcile
+                        that is delayed
+
+                * messages: a list of messages relevant to this route. Each
+                    message is a dictionary containing
+
+                    - priority: the priority level of this message
+                    - text: the content of the message
+
+        '''
         req_params = { 'a': self.agency, 'command': 'predictions',
           'stopId': stop_id }
 
@@ -118,7 +210,7 @@ class Agency:
                         p_dict = { 'epochTime': p_att['epochTime'],
                                    'seconds': p_att['seconds'],
                                    'minutes': p_att['minutes'],
-                                   'isDeparture': p_att['isDeparture'],
+                                   'isDeparture': JAVA_TF_TO_PYTHON_TF[p_att['isDeparture']],
                                    'dirTag': p_att['dirTag'],
                                    'tripTag': p_att['tripTag'],
                                    'affectedByLayover': False,
